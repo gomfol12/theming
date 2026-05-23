98e45b15724c0d53c9ee471fb6aaee260e679bf5
gomfol12 <info@marekb.de>
2026-05-23 10:15:01 +0200

    add generating lua color file

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@ static void generate_colors_xresources(FILE *, vector_t *, void *);
 static void generate_colors(FILE *, vector_t *, void *);
 static void generate_colors_json(FILE *, vector_t *, void *);
 static void generate_colors_js(FILE *, vector_t *, void *);
+static void generate_colors_lua(FILE *, vector_t *, void *);
 static void *pthread_generate_wrapper(void *);
 static void generate_themes(config_t config);
 static void wal_compatibility_helper(config_t, const char *, const char *);
@@ -468,6 +469,37 @@ static void generate_colors_js(FILE *file, vector_t *colors, void *userdata)
                   "};");
 }
 
+static void generate_colors_lua(FILE *file, vector_t *colors, void *userdata)
+{
+    const char *bg = (char *)colors->items[0];
+    const char *fg = (char *)colors->items[7];
+    const char *image_path = (char *)userdata;
+
+    fprintf(file,
+            "return {\n"
+            "    wallpaper = \"%s\",\n"
+            "    alpha = 100,\n"
+            "    special = {\n"
+            "        background = \"%s\",\n"
+            "        foreground = \"%s\",\n"
+            "        cursor = \"%s\"\n"
+            "    },\n"
+            "    colors = {\n",
+            image_path, bg, fg, fg);
+
+    for (size_t i = 0; i < colors->size; i++)
+    {
+        fprintf(file, "        [\"color%d\"] = \"%s\"", (int)i, (char *)colors->items[i]);
+        if (i < colors->size - 1)
+        {
+            fprintf(file, ",");
+        }
+        fprintf(file, "\n");
+    }
+    fprintf(file, "    }\n"
+                  "}");
+}
+
 static void *pthread_generate_wrapper(void *arg)
 {
     command_t *command = (command_t *)arg;
@@ -489,6 +521,7 @@ static void generate_themes(config_t config)
     create_cache_file("colors.scss", vec, config.cache_path, generate_colors_scss, config.image_path);
     create_cache_file("colors-kitty.conf", vec, config.cache_path, generate_colors_kitty_conf, NULL);
     create_cache_file("colors.js", vec, config.cache_path, generate_colors_js, config.image_path);
+    create_cache_file("colors.lua", vec, config.cache_path, generate_colors_lua, config.image_path);
 
     vector_free(vec);
 
